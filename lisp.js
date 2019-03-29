d42ef4375210de2274368ea5bad2239b4259138d
nitin <snitin8994@gmail.com>
2019-03-29 09:10:04 +0530

    Modify define and arithmetic parser

--- lisp.js
+++ lisp.js
@@ -1,7 +1,12 @@
 let result
 const numberParser = input => (result = input.match(/^-?(0|([1-9][0-9]*))(\.[0-9]+)?([E][+-]?[0-9]+)?/i)) && [result[0] * 1, input.slice(result[0].length)]
 const spaceParser = input => input.replace(/^\s+/, '')
-const stringParser = input => (result = input.match(/[a - zA - Z0 - 9] +/) && [result[0], input.slice(result[0].length)])
+const stringParser = input => {
+  result = input.match(/^[a-zA-Z0-9]+/)
+  if (!result) return null
+  if (globalEnv[result[0]]) return [globalEnv[result[0]], input.slice(result[0].length)]
+  return [result[0], input.slice(result[0].length)]
+}
 
 let globalEnv = {
   '+': (args) => args.reduce((a, b) => a + b),
@@ -12,11 +17,12 @@ let globalEnv = {
   '>': (x, y) => x > y,
   '>=': (x, y) => x >= y,
   '<=': (x, y) => x <= y,
-  '=': (x, y) => x === y
+  '=': (x, y) => x === y,
+  'pi': Math.PI
 }
 
 const mathParser = input => {
-  if (!(result = input.match(/^(\+|-|\/|\*)/))) return null
+  if (!(result = input.match(/^(\+|-|\/|\*|>=|<=|>|<|=)/))) return null
   let operation = result[0]
   input = spaceParser(input.slice(1))
   let operands = []
@@ -24,11 +30,35 @@ const mathParser = input => {
     result = expressionParser(input)
     if (!result) return null
     operands.push(result[0])
-    input = result[1]
-    input = spaceParser(input)
+    input = spaceParser(result[1])
   }
-
   return [globalEnv[operation](operands), input]
+}
+
+
+// const ifParser = input => {
+//   if (!input.startsWith('if')) return null
+//   input = spaceParser(input.slice(2))
+//   result = expressionParser(input)
+//   if(!result) return null
+//   input= spaceParser(result[1])
+//   //true
+//   if(result[0]) {
+
+//   }
+
+// }
+
+const defineParser = input => {
+  if (!input.startsWith('define')) return null
+  input = spaceParser(input.slice(6))
+  result = stringParser(input)
+  if (!result) return null
+  let identifier = result[0]
+  let value = expressionParser(result[1])
+  if (!value) return null
+  globalEnv[identifier] = value[0]
+  return [true, spaceParser(value[1])]
 }
 
 const sExpressionParser = input => {
@@ -47,9 +77,11 @@ const sExpressionParser = input => {
 
 const expressionParser = input => {
   input = spaceParser(input)
-  return sExpressionParser(input) || numberParser(input) || mathParser(input)
+  result = sExpressionParser(input) || numberParser(input) || mathParser(input) || defineParser(input) || stringParser(input)
+  if (!result) return null
+  return result
 }
 
-console.log(expressionParser('(+ 1 5 8 (+ 345 67) (+ 6 5) (* 4 5) (- 123 67 ))'))
+console.log(expressionParser('(* 45 6 78 56)'))
 
-//console.log(mathParser('+ )'))
+// console.log(defineParser("define (12333"))
